1b9dad94e71fa3719c8a399bd18749dfdd37e2c7
Staab, Dave <i47@ornl.gov>
2025-07-09 08:41:29 -0400

    specs

--- specs/landing-page.feature
+++ specs/landing-page.feature
@@ -2,5 +2,4 @@ Feature: Landing Page
 
   Scenario: Should have the region title
     Given I am on the landing page
-    When I look at the page
     Then I should see the text "Knoxville"
--- specs/steps.ts
+++ specs/steps.ts
@@ -1,15 +1,11 @@
+import { expect } from '@playwright/test';
 import { createBdd } from 'playwright-bdd';
 const { Given, When, Then } = createBdd();
 
 Given('I am on the landing page', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-});
-
-When('I look at the page', async ({}) => {
-  // Step: When I look at the page
-  // From: specs/landing-page.feature:5:5
+  await page.goto('/');
 });
 
 Then('I should see the text {string}', async ({ page }, text) => {
-  
+  expect(page.getByText(text)).toBeVisible();
 });

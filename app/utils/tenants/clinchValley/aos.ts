ffa53a97c67860a36321e2b01ef7df2be218ea6e
Dave Staab <dave.staab@cadre5.com>
2025-07-26 07:36:42 -0400

    added all cv aos

--- app/utils/tenants/clinchValley/aos.ts
+++ app/utils/tenants/clinchValley/aos.ts
@@ -28,6 +28,32 @@ const lostPearls: AO = {
   mapUrl: 'https://map.f3nation.com/?eventId=49950&locationId=49915',
   aoq: 'Bail Bonds',
 };
+const theBrickYard: AO = {
+  name: 'The Brick Yard',
+  slug: 'the-brick-yard',
+  locationName: 'Powell High School',
+  locationDescription: 'Lower parking lots by playground: look for #shovelflag',
+  aoq: 'Spellcheck',
+  events: [{
+    type: 'Bootcamp',
+    description: 'Tuesday, Thursday 05:30-06:15am',
+  }, {
+    type: 'Bootcamp',
+    description: 'Saturday 06:00-07:00am',
+  }],
+  mapUrl: 'https://map.f3nation.com/?eventId=33902&locationId=33902 ',
+};
+const theProjectVLS: AO = {
+  name: 'The Project VLS',
+  slug: 'the-project-vls',
+  locationName: 'Melton Lake Park',
+  locationDescription: 'Meet at the pavilion',
+  events: [{
+    type: 'Virtuous Leadership School',
+    description: 'Wednesday 5:00AM - 6:00AM',
+  }],
+  aoq: 'Baby Face',
+  mapUrl: 'https://map.f3nation.com/?eventId=50119&locationId=50060',
+};
 
-
-export const AOs: AO[] = [theProject, lostPearls];
+export const AOs: AO[] = [theProject, lostPearls, theBrickYard, theProjectVLS];

1fa635b1ffda316194c865a3aebbcb762e5a57a8
Wanderer <davestaab@gmail.com>
2026-02-15 16:09:41 -0500

    rename clinch valley folder/file

--- app/utils/tenants/index.ts
+++ app/utils/tenants/index.ts
@@ -1,6 +1,6 @@
 import type { TenantConfig, TenantId } from "./types";
 import westKnox from './west-knox/west-knox';
-import clinchValley from "./clinchValley/clinchValley";
+import clinchValley from "./clinch-valley/clinch-valley";
 import test from "./test";
 
 export const tenants: Record<TenantId, TenantConfig> = {
